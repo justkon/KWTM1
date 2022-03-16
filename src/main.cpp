cf1c9f51e6127338d1fa95c25deb6f504f02699e
student <student@Mac-mini.local>
2022-03-16 11:38:35 +0100

    Third commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include"LaborkaConfig.h.in"
 int main(){
-return 0;
+#ifdef USE_TRIGONOMETRY_DEGREE
+double result = cos_degree(45.0);
+#else
+double result = cos(M_PI/4.0); 
+#endif
+double sinus=sin(result);
+return sinus;
 }
